2d378c49e3e03ededa271257ff3b82994071b8a1
diego <caipediego541@gmail.com>
2026-04-10 10:00:02 -0300

    mejoras en la clase estacionamiento

--- estacionamiento.py
+++ estacionamiento.py
@@ -3,45 +3,50 @@ from datetime import datetime
 class Estacionamiento:
     def __init__(self):
         self.capacidad = 50
-        self.tarjetas = []
+        self.tarjetas = {}
         self.registro = {} 
         self.tarifa = {"moto": 1500, "auto": 2000, "camioneta": 3000}
 
     def asignar_lugar(self, id_tarjeta, tipo_vehiculo):
         if len(self.tarjetas) < self.capacidad:
-            self.tarjetas.append(id_tarjeta)
-            self.registro[id_tarjeta] = datetime.now()
-            print("Vehículo con tarjeta", id_tarjeta, "ingresado.")
+             
+            if tipo_vehiculo in self.tarifa:
+                self.tarjetas[id_tarjeta]=tipo_vehiculo
+                self.registro[id_tarjeta] = datetime.now()
+                print(f"Vehículo con tarjeta {id_tarjeta} ingresado.")
+            else: 
+                print("Tipo de vehículo no aceptable.")
         else:
             print("Estacionamiento lleno.")
 
-    def liberar_lugar(self, id_tarjeta, tipo_vehiculo):
+                 
+            
+      
+    def liberar_lugar(self, id_tarjeta):
         if id_tarjeta in self.tarjetas:
             hora_ingreso = self.registro[id_tarjeta]
             hora_salida = datetime.now()
 
             # calcular horas aproximadas
-            horas = (hora_salida.hour + hora_salida.minute/60) - (hora_ingreso.hour + hora_ingreso.minute/60)
+            horas = (hora_salida - hora_ingreso).total_seconds()/3600
             if horas < 1:
                 horas = 1
             else:
                 horas = round(horas)
-
+            tipo_vehiculo=self.tarjetas[id_tarjeta]
             tarifa_total = self.tarifa[tipo_vehiculo] * horas
 
             # eliminar del registro y lista
-            self.tarjetas.remove(id_tarjeta)
+            del self.tarjetas[id_tarjeta]
             del self.registro[id_tarjeta]
 
-            print("Vehículo con tarjeta", id_tarjeta, "retirado.")
-            print("Hora ingreso:", hora_ingreso.hour, "h", hora_ingreso.minute, "min")
-            print("Hora salida:", hora_salida.hour, "h", hora_salida.minute, "min")
-            print("Tarifa total:", tarifa_total)
+            print(f"Vehículo con tarjeta {id_tarjeta} retirado.")
+            print(f"Tarifa total: {tarifa_total} $  | cantidad de horas: {horas}")
         else:
-            print("No se encontró vehículo con tarjeta", id_tarjeta)
+            print(f"No se encontró vehículo con tarjeta:{id_tarjeta}")
 
     def mostrar_vehiculos(self):
         if not self.tarjetas:
             print("No hay vehículos en el estacionamiento.")
         else:
-            print("Tarjetas dentro del estacionamiento:", self.tarjetas)
+            print(f"Tarjetas dentro del estacionamiento:{self.tarjetas}")
